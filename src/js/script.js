0f540e817fd95cdfbc2e074e5dc6069c3df0d861
areseius <areseius@gmail.com>
2024-04-23 23:15:33 +0400

    improvements

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,6 @@ const loading = document.querySelector(".loading");
 const count = document.querySelector(".shoppingCount");
 const basketPage = document.querySelector(".basketPage");
 const basketPageIcon = document.querySelector(".basketPageIcon");
-const close = document.querySelector(".close");
 const basketProducts = document.querySelector(".basketProducts");
 const total = document.querySelector(".total");
 const banner = document.querySelector(".banner");
@@ -16,6 +15,7 @@ const footer = document.querySelector("footer");
 
 const buyedProducts = {};
 let totalAmount = 0;
+let products = [];
 
 // -------------------------------------------------- getting products
 
@@ -28,23 +28,33 @@ const getProducts = async () => {
   return response.json();
 };
 
-// -------------------------------------------------- display produtcs
+// -------------------------------------------------- other useful functions
+
+const calculateBasketAmount = () => {
+  for (const key in buyedProducts)
+    totalAmount += +buyedProducts[key][1].price * +buyedProducts[key][0];
+
+  total.textContent = totalAmount.toFixed(2);
+  totalAmount = 0;
+};
+
+// -------------------------------------------------- upload products to the screen
 
 getProducts().then((data) => {
   setTimeout(() => {
-    const products = data;
+    products = data;
 
     let html = "";
 
-    for (let i = 0; i < products.length; i++) {
+    for (const product of products) {
       html += `<article class="shopCard">
   <figure>
-    <img src="${products[i].url}" alt="${products[i].alt}" />
+    <img src="${product.url}" alt="${product.alt}" />
   </figure>
   <div class="cardInfo">
-    <h1>${products[i].price} ₼</h1>
-    <h2>${products[i].name}</h2>
-    <p>${products[i].desc}</p>
+    <h1>${product.price} ₼</h1>
+    <h2>${product.name}</h2>
+    <p>${product.desc}</p>
   </div>
   <div class="cardButtons">
   <button class="basket">
@@ -66,40 +76,30 @@ getProducts().then((data) => {
   }, 1000);
 });
 
-// -------------------------------------------------- increment basket count
+// -------------------------------------------------- adding product to the basket
 
 home.addEventListener("click", (e) => {
   if (e.target.className == "basketWrapper") {
     count.textContent++;
-    let currentProduct =
-      e.target.parentElement.parentElement.parentElement.children[0].children[0]
-        .alt;
 
-    buyedProducts[currentProduct]
-      ? buyedProducts[currentProduct][0]++
-      : (buyedProducts[currentProduct] = [
-          1,
-          e.target.parentElement.parentElement.parentElement.children[1].children[0].textContent.split(
-            " "
-          )[0],
-          e.target.parentElement.parentElement.parentElement.children[0].children[0].getAttribute(
-            "src"
-          ),
-          e.target.parentElement.parentElement.parentElement.children[0].children[0].getAttribute(
-            "alt"
-          ),
-        ]);
+    const currentProduct =
+      products[
+        [
+          ...e.target.parentElement.parentElement.parentElement.parentElement
+            .children,
+        ].indexOf(e.target.parentElement.parentElement.parentElement)
+      ];
+    console.log(currentProduct);
+    buyedProducts[currentProduct.alt]
+      ? buyedProducts[currentProduct.alt][0]++
+      : (buyedProducts[currentProduct.alt] = [1, currentProduct]);
   }
 });
 
 // -------------------------------------------------- basket page
 
 basketPageIcon.addEventListener("click", () => {
-  for (const key in buyedProducts)
-    totalAmount += +buyedProducts[key][1] * +buyedProducts[key][0];
-
-  total.textContent = totalAmount.toFixed(2);
-  totalAmount = 0;
+  calculateBasketAmount();
 
   basketPage.style.display = "block";
 
@@ -107,10 +107,19 @@ basketPageIcon.addEventListener("click", () => {
 
   for (const key in buyedProducts) {
     html += `<div class="basketProduct">
+    <div class="basketProductInfo">
     <figure>
-      <img src="${buyedProducts[key][2]}" alt="${buyedProducts[key][3]}" />
+      <img src="${buyedProducts[key][1].url}" alt="${buyedProducts[key][1].alt}" />
     </figure>
-    <h1>Ədəd : <span>${buyedProducts[key][0]}</span> x <span>${buyedProducts[key][1]}</span> ₼</h1>
+    <div>
+      <h1>${buyedProducts[key][1].name}</h1>
+      <p>${buyedProducts[key][1].nov}</p>
+    </div>
+</div>
+    <h1>Ədəd : ${buyedProducts[key][0]}</h1>
+
+    <h1>Qiymət : ${buyedProducts[key][1].price} ₼</h1>
+
     <i class="fa-solid fa-trash"></i>
   </div>`;
   }
@@ -118,32 +127,32 @@ basketPageIcon.addEventListener("click", () => {
   basketProducts.innerHTML = html;
 });
 
-close.addEventListener("click", () => {
+basketPage.children[0].children[0].addEventListener("click", () => {
   basketPage.style.display = "none";
 });
 
-// -------------------------------------------------- basket page inner
+// -------------------------------------------------- delete products from basket
 
 basketPage.addEventListener("click", (e) => {
   if (e.target.classList[1] == "fa-trash") {
-    const currentProduct = e.target.parentElement.children[0].children[0].alt;
+    const currentProduct =
+      e.target.parentElement.children[0].children[0].children[0].alt;
 
     if (buyedProducts[currentProduct][0] != 1) {
       buyedProducts[currentProduct][0]--;
       e.target.parentElement.children[1].children[0].textContent--;
       count.textContent--;
       total.textContent = (
-        total.textContent - buyedProducts[currentProduct][1]
+        total.textContent - buyedProducts[currentProduct][1].price
       ).toFixed(2);
     } else {
       e.target.parentElement.remove();
       count.textContent--;
       buyedProducts[currentProduct][0]--;
       total.textContent = (
-        total.textContent - buyedProducts[currentProduct][1]
+        total.textContent - buyedProducts[currentProduct][1].price
       ).toFixed(2);
       delete buyedProducts[currentProduct];
     }
   }
-  console.log(buyedProducts);
 });
